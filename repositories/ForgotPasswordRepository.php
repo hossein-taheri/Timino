6fd11645e9bc5948c5fef95c2e9032c0a267dc57
m.aminr99 <m.aminr99@gmail.com>
2022-03-12 21:06:58 +0330

    add RecordForgotPassword function in ForgotPasswordReposirory

--- repositories/ForgotPasswordRepository.php
+++ repositories/ForgotPasswordRepository.php
@@ -11,4 +11,16 @@ class ForgotPasswordRepository {
         $statement->execute();
         return $statement->fetchAll()[0];
     }
+
+
+    public static function RecordForgotPassword($email,$verified_code,$expires_at){
+    $pdo = $GLOBALS['pdo'];
+    $query = "INSERT INTO forgot_passwords (eamil,verified_code,expires_at) VALUES (:eamil,:verified_code,:expires_at)";
+    $statement = $pdo->prepare($query);
+    $statement->bindParam(":email", $email);
+    $statement->bindParam(":verified_code", $verified_code);
+    $statement->bindParam(":expires_at", $expires_at);
+    $statement->execute();
+    return $statement->fetchAll()[0];
+   }
 }
