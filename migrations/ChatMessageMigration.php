184865c4a2464e181175c7140ca9f118799e9de7
Hossein <you@example.com>
2022-03-29 18:54:30 +0430

    TimelineMember and ChatMessage have been added

--- migrations/ChatMessageMigration.php
+++ migrations/ChatMessageMigration.php
@@ -2,21 +2,19 @@
 
 namespace Migration;
 
-class TimeLineMigration
+class ChatMessageMigration
 {
     public static function create()
     {
         $pdo = $GLOBALS['pdo'];
         $pdo->query("
-            CREATE TABLE IF NOT EXISTS `timelines`(
+            CREATE TABLE IF NOT EXISTS `timeline`(
                 `id` int(10) NOT NULL auto_increment,
+                `timeline_id` int(10) NOT NULL,
                 `user_id` int(10) NOT NULL,
-                `title` varchar(250) NOT NULL,
-                `description` varchar(250),
-                `avatar` varchar(250),
-                `privilege_level` ENUM('private','public') NOT NULL DEFAULT 'private',
+                `message` varchar(500) NOT NULL,
                 `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,     
-                PRIMARY KEY (`id`),
+                FOREIGN KEY (`timeline_id`) REFERENCES timelines(`id`),
                 FOREIGN KEY (`user_id`) REFERENCES users(`id`) 
             );
         ");
---
+++ migrations/TimelineMemberMigration.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Migration;
+
+class TimelineMemberMigration
+{
+    public static function create()
+    {
+        $pdo = $GLOBALS['pdo'];
+        $pdo->query("
+            CREATE TABLE IF NOT EXISTS `timeline`(
+                `timeline_id` int(10) NOT NULL,
+                `user_id` int(10) NOT NULL,
+                `event_privilege_level` ENUM('read_only','create_event') NOT NULL DEFAULT 'read_only',
+                `chat_access` BOOL NOT NULL DEFAULT FALSE,     
+                `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,     
+                FOREIGN KEY (`timeline_id`) REFERENCES timelines(`id`),
+                FOREIGN KEY (`user_id`) REFERENCES users(`id`) 
+            );
+        ");
+    }
+}
