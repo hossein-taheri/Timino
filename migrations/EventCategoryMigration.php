820d39789bb45f1781bbfd344ed61bd29b9c816c
Hossein <you@example.com>
2022-05-30 13:15:57 +0430

    Date in event has been added

--- migrations/EventCategoryMigration.php
+++ migrations/EventCategoryMigration.php
@@ -9,8 +9,10 @@ class EventCategoryMigration
         $pdo = $GLOBALS['pdo'];
         $pdo->query("
             CREATE TABLE IF NOT EXISTS `event_categories`(
+                `id` int(10) NOT NULL auto_increment,
                 `event_id` int(10) NOT NULL,
                 `category_id` int(10) NOT NULL,
+                PRIMARY KEY  (`id`),
                 FOREIGN KEY (`event_id`) REFERENCES events(`id`),
                 FOREIGN KEY (`category_id`) REFERENCES categories(`id`) 
             );
--- migrations/TimelineMemberMigration.php
+++ migrations/TimelineMemberMigration.php
@@ -9,11 +9,13 @@ class TimelineMemberMigration
         $pdo = $GLOBALS['pdo'];
         $pdo->query("
             CREATE TABLE IF NOT EXISTS `timeline_members`(
+                `id` int(10) NOT NULL auto_increment,
                 `timeline_id` int(10) NOT NULL,
                 `user_id` int(10) NOT NULL,
                 `event_privilege_level` ENUM('read_only','create_event') NOT NULL DEFAULT 'read_only',
-                `chat_access` BOOL NOT NULL DEFAULT FALSE,     
+                `chat_access` BOOLEAN NOT NULL DEFAULT FALSE,     
                 `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,     
+                PRIMARY KEY  (`id`),
                 FOREIGN KEY (`timeline_id`) REFERENCES timelines(`id`),
                 FOREIGN KEY (`user_id`) REFERENCES users(`id`) 
             );
